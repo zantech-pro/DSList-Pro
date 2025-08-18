42882e325d24f8f698df9311c30229340ceb5378
Zantech <dev@zantech.com.br>
2025-08-18 20:08:28 -0300

    Populado a tabela tb_game com dados de teste

--- src/main/java/br/com/zantetech/dslist/entities/Game.java
+++ src/main/java/br/com/zantetech/dslist/entities/Game.java
@@ -21,16 +21,20 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private double score;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
@@ -84,11 +88,11 @@ public class Game {
 		this.platforms = platforms;
 	}
 
-	public double getScore() {
+	public Double getScore() {
 		return score;
 	}
 
-	public void setScore(double score) {
+	public void setScore(Double score) {
 		this.score = score;
 	}
 
