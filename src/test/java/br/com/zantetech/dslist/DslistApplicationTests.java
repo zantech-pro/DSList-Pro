d83eeff8fe59587b6348efde62d9320d22d9661d
Zantech <dev@zantech.com.br>
2025-08-18 18:55:53 -0300

    Projeto Criado

---
+++ src/test/java/br/com/zantetech/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zantetech.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
